f8ac067c53d5c2d8b50cdfce30c1981269ba4b19
IDGS-805-22001749 <oscarvgz07@gmail.com>
2025-03-04 23:40:54 -0600

    Agregar

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from config import DevelopmentConfig
 from models import db
 from models import Alumnos
 
+import forms
+
+
+
+
+
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
@@ -15,10 +21,46 @@ csrf=CSRFProtect()
 def page_not_found(e):
     return render_template('404.html'),404
 
-@app.route("/")
-@app.route("/index")
+
+
+
+
+@app.route("/", methods=['GET','POST'])
+@app.route ("/index")
 def index():
-	return render_template("index.html")
+     create_form=forms.UserForm2(request.form)
+
+     alumno=Alumnos.query.all()
+
+     return render_template("index.html", form=create_form,alumnos=alumno)
+
+
+@app.route("/detalles", methods=['GET','POST'])
+def detalles():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        nombre=alum1.nombre
+        apaterno=alum1.apaterno
+        email=alum1.email
+    return render_template("detalles.html",form=create_form,nombre=nombre,apaterno=apaterno,email=email)
+
+
+
+@app.route('/Alumnos1', methods=['GET','POST'])
+def Alumnos1():
+    create_form=forms.UserForm2(request.form)
+    if request.method == 'POST':
+        alum=Alumnos(nombre=create_form.nombre.data,
+                     apaterno=create_form.apaterno.data,
+                     email=create_form.email.data)
+        #insert alumnos() values()
+
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('alumnos.html', form = create_form)
 
 if __name__ == '__main__':
     csrf.init_app(app)
--- models.py
+++ models.py
@@ -6,8 +6,8 @@ db=SQLAlchemy()
 class Alumnos(db.Model):
     __tablename__ = 'alumnos'
     id=db.Column(db.Integer,primary_key=True)
-    nombre=db.Column(db.String(59))
-    apaterno=db.Column(db.String(59))
-    email=db.Column(db.String(59))
+    nombre=db.Column(db.String(50))
+    apaterno=db.Column(db.String(50))
+    email=db.Column(db.String(50))
     create_date=db.Column(db.DateTime, default=datetime.datetime.now)
 
